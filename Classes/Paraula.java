7a8f46198814ef3d7a72f987f1728bed06d490ea
BernatBC <bernat.borrasc@gmail.com>
2022-10-15 16:28:40 +0200

    Afegit classe Paraula

---
+++ Classes/Paraula.java
@@ -0,0 +1,87 @@
+import java.io.*;
+
+/** Paraula que pot ser continguda a algun document.
+ * @author Bernat Borràs Civil (bernat.borras.civil@estudiantat.upc.edu)
+ */
+public class Paraula {
+
+    /** Valor de la mètrica idf assignat. */
+    private Double idf;
+
+    /** Mot repressentat per la classe. */
+    private String mot;
+
+    /** Número d'ocurrencies. */
+    private Int ocurrencia;
+
+    /** Constructora per defecte de paraula */
+    public Paraula() {
+        mot = "";
+        ocurrencia = 0;
+    }
+
+    /** Constructora per defecte de paraula
+     *  @param p Paraula representada per la classe.
+    */
+    public Paraula(String p) {
+        mot = p;
+        ocurrencia = 1;
+    }
+
+    /** Retorna el nombre d'ocurrencies total.
+     * @return Int : Nombre d'ocurrencies.
+     */
+    public Int getOcurrencia() {
+        return ocurrencia;
+    }
+
+    /** Retorna la paraula representada per la classe.
+     * @return String : Paraula.
+     */
+    public String getParaula() {
+        return mot;
+    }
+
+    /** Assigna el mot repressentat per la classe.
+     * @param p Paraula representada per la classe
+     */
+    public void setParaula(String p) {
+        mot = p;
+        ocurrencia = 1;
+    }
+
+    /** Assigna el nombre d'ocurrencies.
+     * @param n Nombre d'ocurrencies.
+     */
+    public void setOcurrencia(Int n) {
+        ocurrencia = n;
+    }
+
+    /** Decrementa en n unitats el nombre d'ocurrencies.
+     * @param n Nombre d'ocurrencies.
+     */
+    public void decrementarOcurrencia(Int n) {
+        ocurrencia -= n;
+        if (n < 0) {
+            n = 0;
+            System.out.println("ERROR: Ocurrencies negatives a la paraula " + mot);
+        }
+    }
+
+    /** Incrementa en n unitats el nombre d'ocurrencies.
+     * @param n Nombre d'ocurrencies.
+     */
+    public void incrementarOcurrencia(Int n) {
+        ocurrencia += n;
+    }
+
+    /** Calcula el valor idf de la paraula.
+     */
+    private void calcularIdf() {
+        //idf = log(nDocumentsTotals/nDocumentsOnApareix)
+        //Necessita:
+        //getNDocuments()
+        //Desar d'alguna manera els documents on la paraula apareix
+    }
+
+}
