4cc849562c505ed74752237a7e3210383dfe4683
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-10-25 17:40:32 +0200

    consultaAutor()

--- Classes/ConsultaAutor.java
+++ Classes/ConsultaAutor.java
@@ -20,21 +20,38 @@ import Frase.java;
      * @param d document que es vol afegir.
      */
     public void afegirDocument(Document d) {
-
+        Frase autor = d.getAutor().enString();
+        Llibreria l = coleccions.get(autor);
+        if (l == NULL) l = new Llibreria();
+        l.addDocument(d);
+        coleccions.put(autor, l);
     }
 
     /** Elimina el document de la llibreria corresponent.
-     * @param d document que es vol afegir.
+     * @param d document que es vol eliminar.
      */
     public void eliminarDocument(Document d) {
-
+        Frase autor = d.getAutor().enString();
+        Llibreria l = coleccions.get(autor);
+        l.deleteDocument(d);
+        if (l.getNDocuments() != 0) coleccions.put(autor, l);
+        else coleccions.remove(autor);
     }
 
     /** Mou el document cap a la llibreria de l'autor nou.
-     * @param d document que es vol afegir.
-     * @param nou_autor autor que passarà a tenir el document.
+     * @param antic document que es vol afegir.
+     * @param nou autor que passarà a tenir el document.
      */
-    public void canviarAutor(Document d, Frase nou_autor) {
+    public void canviarAutor(Document antic, Document nou) {
+        eliminarDocument(antic);
+        afegirDocument(nou);
+    }
 
+    /** Retorna la llibreria corresponent a l'autor.
+     * @param autor autor dels documents retornats.
+     * @return Llibreria : llibreria corresponent a l'autor.
+     */
+    public Llibreria getDocAutor(Frase autor) {
+        return coleccions.get(autor.enString());
     }
  }
