cd786679e8b8e91f4ee05c3b60c5863ffa1fcd10
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-11-06 10:06:38 +0100

    Afegir consultes main

--- Classes/Main.java
+++ Classes/Main.java
@@ -3,6 +3,14 @@ import Classes.Llibreria;
 import Classes.Vocabulari;
 import Classes.DocumentCtrl;
 
+import javax.swing.text.Document;
+import java.time.LocalDate;
+
+import Classes.ConsultaData;
+import Classes.ConsultaPreferit;
+import Classes.ConsultaRellevancia;
+import Classes.ConsultaTitol;
+
 /** Main.
  * @author Bernat Borràs Civil (bernat.borras.civil@estudiantat.upc.edu)
  */
@@ -10,11 +18,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ConsultaData CD = new ConsultaData();
+        ConsultaTitol CT = new ConsultaTitol();
+        ConsultaPreferit CP = new ConsultaPreferit();
+
         Vocabulari v = new Vocabulari();
         Llibreria l = new Llibreria();
         DocumentCtrl DCtlr = new DocumentCtrl(v, l);
 
         DCtlr.crearDocument();
+
+        Document doc1 = DCtlr.getDocument("John Doe", "Document Test").getR();
+
+        Frase autor = new Frase("John Doe");
+        
+        Llibreria l1 = CD.getAnterior(LocalDate.now());
+        Llibreria l2 = CP.getDocPreferit();
+        Llibreria l3 = CT.getDocAutor(autor);
 
         while (true) ;
     }
