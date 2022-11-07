99579612885ef4b6456d553b4d4d46a24d9b4c50
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-11-07 19:21:09 +0100

    Remove unused functions + start ParaulaTest.java

--- classes/Paraula.java
+++ classes/Paraula.java
@@ -12,9 +12,6 @@ public class Paraula {
     /** Número d'ocurrencies. */
     private int ocurrencia;
 
-    /** Número de documents on la paraula apareix. */
-    private int nDocuments;
-
     /** Identificador de la paraula. */
     private int index;
 
@@ -26,16 +23,8 @@ public class Paraula {
     public Paraula(String p) {
         mot = p;
         ocurrencia = 1;
-        nDocuments = 0;
         index = proxim_index;
         ++proxim_index;
-    }
-
-    /** Retorna el número de documents on la paraula apareix.
-    * @return Double : Número de documents.
-    */
-    public int getNumDocuments() {
-        return nDocuments;
     }
 
     /** Retorna el nombre d'ocurrencies total.
@@ -50,21 +39,6 @@ public class Paraula {
      */
     public String getParaula() {
         return mot;
-    }
-
-    /** Assigna el mot repressentat per la classe.
-     * @param p Paraula representada per la classe
-     */
-    public void setParaula(String p) {
-        mot = p;
-        ocurrencia = 1;
-    }
-
-    /** Assigna el nombre d'ocurrencies.
-     * @param n Nombre d'ocurrencies.
-     */
-    public void setOcurrencia(int n) {
-        ocurrencia = n;
     }
 
     /** Decrementa en n unitats el nombre d'ocurrencies.
@@ -84,17 +58,6 @@ public class Paraula {
     public void incrementarOcurrencia(int n) {
         ocurrencia += n;
     }
-
-    /** Decrementa en una unitat el número de documents on la paraula apareix. */
-    public void decrementarNumDocuments() {
-        --nDocuments;
-    }
-
-    /** Incrementa en una unitat el número de documents on la paraula apareix. */
-    public void incrementarNumDocuments() {
-        ++nDocuments;
-    }
-
 
     /** Retorna l'índex de la paraula.
      * @return int : índex de la paraula.
---
+++ test/classes/ParaulaTest.java
@@ -0,0 +1,12 @@
+package test.classes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class ParaulaTest {
+    
+    @Before                                         
+    public void setUp() {
+        System.out.println("SetUp Completed!");
+    }    
+}
