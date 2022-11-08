9c0850ac9d9c473fd8086aa14a853f816201aeec
Loparc <oscarlopingo@gmail.com>
2022-11-08 18:22:25 +0100

    Ternary Tree Autors used

--- classes/ConsultaAutors.java
+++ classes/ConsultaAutors.java
@@ -8,24 +8,40 @@ import java.util.Set;
 public class ConsultaAutors {
     
     /** Arbre de cerca */
-    
+    private TernaryTreeAutor autors;
 
 
+    public ConsultaAutors() {
+        autors = new TernaryTreeAutor();
+    }
 
+    /** Insereix l'autor en l'arbre de cerca per prefix
+     * @param autor
+     */
     public void addAutor(Frase autor) {
-        //insert autor
+        String nom_cognom = autor.toString();
+        while (nom_cognom.length() > 0) {
+            //inserim l'autor
+            autors.inserirAutor(autor, nom_cognom, 0);
+            //esborrem la primera paraula de l'estring
+            int ini = 0;
+            int length = nom_cognom.length();
+            while (ini < length && Character.isLetter(nom_cognom.charAt(ini))) ++ini;
+            if (ini >= length) break;
+            else {
+                //vol dir que hem trobat un espai
+                ++ini;
+                nom_cognom = nom_cognom.substring(ini, length);
+            }
+        }
     }
 
-    public void deleteAutor(Frase autor) {
-        //delete
-    }
-
+    /** Funcio consulta. Retorna els autors els quals compleixen el prefix
+     * @param prefix
+     */
     public Set<Frase> donaAutors(String prefix) {
         //retorna el Set d'autors que compleixen el prefix
-        Set<Frase> autors = new Set<Frase>();
-
-
-
-        return autors;
+        Set<Frase> result = autors.obtenirAutors(prefix, 0);
+        return result;
     }
 }
