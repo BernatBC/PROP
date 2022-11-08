c953a4651392e4e6423b20f85989079300c814ca
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-11-08 17:43:07 +0100

    Fix #16

---
+++ classes/ControladorPresentacio.java
@@ -0,0 +1,12 @@
+package classes;
+
+/** Controlador presentació.
+ * @author Bernat Borràs Civil (bernat.borras.civil@estudiantat.upc.edu)
+ */
+public class ControladorPresentacio {
+    
+    /**Constructora per defecte */
+    public ControladorPresentacio() {
+
+    }
+}
--- classes/Main.java
+++ classes/Main.java
@@ -7,25 +7,6 @@ package classes;
 public class Main {
 
     public static void main(String[] args) {
-
-        ConsultaData CD = new ConsultaData();
-        ConsultaTitol CT = new ConsultaTitol();
-        ConsultaPreferit CP = new ConsultaPreferit();
-
-        Vocabulari v = new Vocabulari();
-        Llibreria l = new Llibreria();
-        DocumentCtrl DCtlr = new DocumentCtrl(v, l);
-
-        DCtlr.crearDocument();
-        /*
-        Document doc1 = DCtlr.getDocument("John Doe", "Document Test").getR();
-
-        Frase autor = new Frase("John Doe");
-        
-        Llibreria l1 = CD.getAnterior(LocalDate.now());
-        Llibreria l2 = CP.getDocPreferit();
-        Llibreria l3 = CT.getDocAutor(autor);*/
-
-        while (true) ;
+        ControladorPresentacio controladorP = new ControladorPresentacio();
     }
 }
