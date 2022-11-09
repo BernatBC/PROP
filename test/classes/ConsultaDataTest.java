0b85246d0d8bb34707224003cdaa597350db3a09
Loparc <oscarlopingo@gmail.com>
2022-11-09 14:56:37 +0100

    started test Data i Rellevancia

---
+++ test/classes/ConsultaDataTest.java
@@ -0,0 +1,19 @@
+package test.classes;
+
+import classes.ConsultaData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class ConsultaDataTest {
+    
+    @Before                                         
+    public void setUp() {
+        System.out.println("SetUp Completed!");
+    }
+
+    
+}
---
+++ test/classes/ConsultaRellevanciaTest.java
@@ -0,0 +1,19 @@
+package test.classes;
+
+import classes.ConsultaRellevancia;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class ConsultaRellevanciaTest {
+    
+    @Before                                         
+    public void setUp() {
+        System.out.println("SetUp Completed!");
+    }
+
+    
+}
