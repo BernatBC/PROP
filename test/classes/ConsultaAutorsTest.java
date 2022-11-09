90c25b30e5b3c6f5ecb119f929b9d166480861d3
Loparc <oscarlopingo@gmail.com>
2022-11-09 13:18:16 +0100

    Consulta Prefix Autors tested correctly :)

--- test/classes/ConsultaAutorsTest.java
+++ test/classes/ConsultaAutorsTest.java
@@ -5,17 +5,15 @@ import classes.Frase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
-
-//import javax.naming.InitialContext;
-//import javax.swing.text.html.HTMLDocument.HTMLReader.ParagraphAction;
 
 import org.junit.*;
 import org.junit.jupiter.api.DisplayName;
 
-/** Classe de proves d'un Contingut.
- * @author Alexandre Ros i Roger (alexandre.ros.roger@estudiantat.upc.edu)
- */
+import java.util.HashSet;
+import java.util.Set;
+
+
+
 public class ConsultaAutorsTest {
     
     @Before                                         
@@ -30,17 +28,46 @@ public class ConsultaAutorsTest {
         // Init
         ConsultaAutors autors = new ConsultaAutors();
 
-        autors.addAutor(new Frase("Oscar Ramos"));
-        autors.addAutor(new Frase("Salvador Roura"));
-        autors.addAutor(new Frase("Manel Macarra"));
-        autors.addAutor(new Frase("Norman Norman"));
-        autors.addAutor(new Frase("Pol Ramos"));
-        autors.addAutor(new Frase("Manel Ramos"));
+        Frase q1 = new Frase("Oscar Ramos");
+        Frase q2 = new Frase("Salvador Roura");
+        Frase q3 = new Frase("Manel Macarra");
+        Frase q4 = new Frase("Norman Norman");
+        Frase q5 = new Frase("Pol Ramos");
+        Frase q6 = new Frase("Manel Ramos");
+        autors.addAutor(q1);
+        autors.addAutor(q2);
+        autors.addAutor(q3);
+        autors.addAutor(q4);
+        autors.addAutor(q5);
+        autors.addAutor(q6);
+
 
 
 
 
         //ASSERT
-        System.out.println(autors.donaAutors("nor"));
+        Set<Frase> result = new HashSet<>();
+        result.add(q6); //Manel Ramos
+        result.add(q3); //Manel Macarra
+        assertEquals(result, autors.donaAutors("Man"));
+
+        //veiem que els case sensitive, els noms propis i cognoms tenen sempre la primera lletra Majuscula
+        assertEquals(null, autors.donaAutors("man"));
+
+
+        //provem ara pel cognom Ramos, nomes agafant Ra
+        result = new HashSet<>();
+        result.add(q1); //Oscar Ramos
+        result.add(q5); //Pol Ramos
+        result.add(q6); //Manel Ramos
+        assertEquals(result, autors.donaAutors("Ra"));
+
+        //Cas nomes R, resultat igual que l'anterior cas però ara s'afegeix salvador Roura
+        result.add(q2); //Salvador Roura
+        assertEquals(result, autors.donaAutors("R"));
+
+        //cas prefix null o cas prefix espai
+        assertEquals(null, autors.donaAutors(""));
+        assertEquals(null, autors.donaAutors(" "));
     }
 }
