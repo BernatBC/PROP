e3889efc81e52e1cb6d47c4385b209ee5da7542f
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-11-13 08:53:58 +0100

    Documentació ConsultaPreferitTest

--- test/classes/ConsultaPreferitsTest.java
+++ test/classes/ConsultaPreferitsTest.java
@@ -26,7 +26,7 @@ public class ConsultaPreferitsTest {
     @Test
     @DisplayName("Test Preferits")   
     public void ConsultaPreferits() {
-        // Init
+        //Inicialitzem la classe consultaPreferit
         ConsultaPreferit consulta = new ConsultaPreferit();
 
         Frase test[] = new Frase[1];
@@ -40,23 +40,33 @@ public class ConsultaPreferitsTest {
 
         Set<Document> esperat = new HashSet<>();
 
+        //Ens retorna un conjunt buit de documents.
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Marquem un document com a preferit i veiem que al fer la consulta ens el retorna.
         consulta.afegirDocument(d1);
         esperat.add(d1);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Quan el tornem a afegir, la consulta es comporta de la mateixa manera.
+        consulta.afegirDocument(d1);
+        assertEquals(esperat, consulta.getDocPreferit());
+        
+        //Eliminem el document, i ja no apareix a la consulta.
         consulta.eliminarDocument(d1);
         esperat.remove(d1);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Quan el tornem a eliminar, segueix comportant-se com esperàvem.
         consulta.eliminarDocument(d1);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //El tornem a afegir, i comprovem que s'afageixi correctament.
         consulta.afegirDocument(d1);
         esperat.add(d1);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Afegim més documents.
         consulta.afegirDocument(d2);
         esperat.add(d2);
         assertEquals(esperat, consulta.getDocPreferit());
@@ -69,6 +79,7 @@ public class ConsultaPreferitsTest {
         esperat.add(d5);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Eliminem aguns documents i seguim comprovant que el resultat sigui correcte.
         consulta.eliminarDocument(d3);
         esperat.remove(d3);
         assertEquals(esperat, consulta.getDocPreferit());
@@ -81,6 +92,7 @@ public class ConsultaPreferitsTest {
         esperat.remove(d2);
         assertEquals(esperat, consulta.getDocPreferit());
 
+        //Eliminem i tornem a afegir algun document més.
         consulta.afegirDocument(d4);
         esperat.add(d4);
         assertEquals(esperat, consulta.getDocPreferit());
