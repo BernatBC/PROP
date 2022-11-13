7a44afdf467a890ca062a664a7fbb8d665201690
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-11-13 17:08:11 +0100

    Add all JUnit tests

---
+++ EXE/JUnitConsultaPreferits/ConsultaPreferitsTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ../../FONT
+rm ./test/classes/*.class
+javac -cp ./lib/junit-platform-console-standalone-1.9.1.jar:. ./test/classes/ConsultaPreferitsTest.java
+java -jar ./lib/junit-platform-console-standalone-1.9.1.jar -cp ./test/classes/ --scan-classpath
