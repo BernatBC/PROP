d737b42c652174feafe4ec7afc05def0723b6b2b
alexland7219 <alexland7219@gmail.com>
2022-11-14 16:35:25 +0100

    Tests from terminal working

--- EXE/JUnitParaula/ParaulaTest.sh
+++ EXE/JUnitParaula/ParaulaTest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd ../../FONT
-rm ./test/classes/*.class
-javac -cp ./lib/junit-platform-console-standalone-1.9.1.jar:. ./test/classes/ParaulaTest.java
-java -jar ./lib/junit-platform-console-standalone-1.9.1.jar -cp ./test/classes/ --scan-classpath
+rm test/classes/*.class classes/*.class
+rm -r out/
+
+mkdir out
+javac -d out classes/*.java
+javac -d out -cp out:lib/junit-platform-console-standalone-1.9.1.jar test/classes/ParaulaTest.java
+java -jar lib/junit-platform-console-standalone-1.9.1.jar --class-path out --scan-class-path
