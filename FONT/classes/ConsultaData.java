a836ae296cdebea449dabdbaa3f6ec70972a6312
Bernat Borràs Civil <bernat.borras.civil@estudiantat.upc.edu>
2022-12-13 16:27:13 +0100

    Delete innecessary method

--- FONT/classes/ConsultaData.java
+++ FONT/classes/ConsultaData.java
@@ -10,19 +10,15 @@ public class ConsultaData {
 
     /** Consulta per l'interval. Ídem que la consulta sense paràmetres però amb la possibilitat de posar nous paràmetres en la pròpia crida. 
      * @return ArrayList de documents els quals la seva data és igual o posterior a "anterior" i igual o anterior a "posterior".
-     * @param ant
-     * @param pos
+     * @param anterior fita superior de l'interval.
+     * @param posterior fita inferior de l'interval.
      */
-    public static ArrayList<Document> consulta(ArrayList<Document> documents, LocalDate ant, LocalDate pos) {
-        return retornarDocs(documents, ant, pos);
-    }
-
-    private static ArrayList<Document> retornarDocs(ArrayList<Document> documents, LocalDate anterior, LocalDate posetrior) {
+    public static ArrayList<Document> consulta(ArrayList<Document> documents, LocalDate anterior, LocalDate posterior) {
         int i = 0;
         int j = documents.size() - 1;
 
         while (i < documents.size() && documents.get(i).getData().isBefore(anterior)) ++i;
-        while (j >= 0 && documents.get(j).getData().isAfter(posetrior)) --j;
+        while (j >= 0 && documents.get(j).getData().isAfter(posterior)) --j;
 
         ArrayList<Document> interval = new ArrayList<>();
 
